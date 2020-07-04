0ea117fdd36d4209061750572ee06e9416f43771
Dyn0402 <Dyn0402@hotmail.com>
2020-07-03 18:33:01 -0700

    Renamed dca_xy_avg_err to dca_xy_sd in nsmEvent

--- StRoot/MyAnalysisMaker/nsmEvent.cxx
+++ StRoot/MyAnalysisMaker/nsmEvent.cxx
@@ -7,7 +7,7 @@ nsmEvent::nsmEvent()
     vtx_y			= 0.;
     vtx_z			= 0.;
     dca_xy_avg		= 0.;
-    dca_xy_avg_err	= 0.;
+    dca_xy_sd	= 0.;
     Nprim 			= 0;
     run   			= 0;
     event_id    	= 0;
@@ -18,7 +18,7 @@ nsmEvent::nsmEvent()
     qy          	= 0.;
 }
 
-nsmEvent::nsmEvent(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_xy_avg, Float_t mdca_xy_avg_err,
+nsmEvent::nsmEvent(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_xy_avg, Float_t mdca_xy_sd,
                    UInt_t mNprim, UInt_t mrun, UInt_t mevent_id, UInt_t mref2, UInt_t mref3, UInt_t mbtof,
 				   Float_t mqx, Float_t mqy)
 {
@@ -27,7 +27,7 @@ nsmEvent::nsmEvent(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_
     vtx_z = mvtx_z;
     
     dca_xy_avg = mdca_xy_avg;
-    dca_xy_avg_err = mdca_xy_avg_err;
+    dca_xy_sd = mdca_xy_sd;
 
     Nprim = mNprim;
     run  = mrun;
@@ -42,7 +42,7 @@ nsmEvent::nsmEvent(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_
     qy = mqy;
 }
 
-void nsmEvent::SetEventData(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_xy_avg, Float_t mdca_xy_avg_err,
+void nsmEvent::SetEventData(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Float_t mdca_xy_avg, Float_t mdca_xy_sd,
                             UInt_t mNprim, UInt_t mrun, UInt_t mevent_id, UInt_t mref2, UInt_t mref3, UInt_t mbtof,
 							Float_t mqx, Float_t mqy
                             )
@@ -52,7 +52,7 @@ void nsmEvent::SetEventData(Float_t mvtx_x, Float_t mvtx_y, Float_t mvtx_z, Floa
     vtx_z = mvtx_z;
     
     dca_xy_avg = mdca_xy_avg;
-    dca_xy_avg_err = mdca_xy_avg_err;
+    dca_xy_sd = mdca_xy_sd;
 
     Nprim = mNprim;
     run  = mrun;
