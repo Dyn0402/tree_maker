84c4f2da0dd183b548d649df6e0192286796c191
Dyn0402 <Dyn0402@hotmail.com>
2020-07-20 19:26:05 -0700

    Make_Trees pass input_file_list.data() instead of string

--- Make_Trees.cpp
+++ Make_Trees.cpp
@@ -34,7 +34,7 @@ void Make_Trees(string input_file_list, string output_dir, int energy) {
 	gSystem->Load("StBTofUtil");
 
 	StChain *chain = new StChain;
-	StMuDstMaker *muDst_maker = new StMuDstMaker(0, 0, "", input_file_list, "MuDst", num_files);
+	StMuDstMaker *muDst_maker = new StMuDstMaker(0, 0, "", input_file_list.data(), "MuDst", num_files);
 
 	// Turn off everything but Primary tracks in order to speed up the analysis and eliminate IO
 	muDst_maker->SetStatus("*", 0);  // Turn off all branches
