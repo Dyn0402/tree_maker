37775b8810e1b5602d18281bca1caea3c73b96f6
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-02-07 16:21:45 -0800

    Flattener added debug out to see if breaking there

--- StRoot/TreeMaker/Flattener.cpp
+++ StRoot/TreeMaker/Flattener.cpp
@@ -208,6 +208,7 @@ void Flattener::calc_ep_terms(string ep_type, int cent_bin, int run, float psi) 
 
 // Get shifted phi given original phi based on Fourier coefficients for specific event/track
 float Flattener::get_flat_phi(float phi, string particle_type, int cent_bin, float eta, int run) {
+	cout << "get_flat_phi" << endl;
 	int eta_bin = get_eta_bin(eta);
 	int run_key = get_run_bin_key(run);
 	if (!phi_sin_terms[particle_type][cent_bin][eta_bin][run_key]) {  // Hopefully just means specific run doesn't exist
